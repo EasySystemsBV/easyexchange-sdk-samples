392f40c5f78a32da0b2072c5e9daf3071e8ca376
Anton Maters <amaters@icreative.nl>
2018-10-09 12:12:42 +0200

    stripped down testcode and cleaned up sample

--- sample/Program.cs
+++ sample/Program.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Threading.Tasks;
-using EasyExchange.SalesInvoiceServer.Sdk.Models.GetInvoices;
+﻿using EasyExchange.Sdk.Sample.SalesInvoices;
 
 namespace EasyExchange.Sdk.Sample
 {
     class Program
     {
-        //private const string ENDPOINT_URL = "https://easyexchange.es-dev.net";
-        private const string ENDPOINT_URL = "http://localhost:30927";
+        private const string ENDPOINT_URL = "https://easyexchange.es-dev.net";
         private const string CLIENT_ID = "CLIENT_ID";
         private const string CLIENT_SECRET = "CLIENT_SECRET";
         private const string USERNAME = "username";
@@ -16,18 +13,9 @@ namespace EasyExchange.Sdk.Sample
         static void Main(string[] args)
         {
             Client client = new Client(ENDPOINT_URL, CLIENT_ID, CLIENT_SECRET, EasyExchangeScope.SalesInvoiceServer);
-            //Client client = new Client(CLIENT_ID, CLIENT_SECRET, EasyExchangeScope.SalesInvoiceServer, new LocalUriBuilder());
             client.Authenticate( USERNAME, PASSWORD ).Wait();
 
-            string emailAddress ="name@example.com";
-            DateTime startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month,1) ;
-
-            Task<GetInvoicesResponse> task = Task.Run(
-                async () => await client.SalesInvoices.GetInvoicesAsync( emailAddress, startDate, null ) );
-            foreach( GetInvoice invoice in task.Result.Data )
-            {
-                Console.WriteLine( $"Invoice ID: ", invoice.Identifier );
-            }
+            GetInvoices.Execute( client );
         }
     }
 }
---
+++ sample/SalesInvoices/GetInvoices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyExchange.SalesInvoiceServer.Sdk.Models.GetInvoices;
+
+namespace EasyExchange.Sdk.Sample.SalesInvoices
+{
+    internal class GetInvoices
+    {
+
+        internal static void Execute( Client client )
+        {
+            string emailAddress = "name@example.com";
+            DateTime startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+
+            Task<GetInvoicesResponse> task = Task.Run(
+                async () => await client.SalesInvoices.GetInvoicesAsync(emailAddress, startDate, null));
+            foreach (GetInvoice invoice in task.Result.Data)
+            {
+                Console.WriteLine($"Invoice ID: ", invoice.Identifier);
+            }
+        }
+    }
+}
