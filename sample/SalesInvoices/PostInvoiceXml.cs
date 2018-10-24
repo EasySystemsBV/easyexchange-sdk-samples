d60bbbd9aeb048f8fc1587aa9e980378da60824f
Anton Maters <amaters@icreative.nl>
2018-10-24 10:40:01 +0200

    voorbeeld bijgewerkt voor PostXml in SalesInvoices

---
+++ sample/SalesInvoices/PostInvoiceXml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyExchange.SalesInvoiceServer.Sdk.Models.PostInvoiceXml;
+
+namespace EasyExchange.Sdk.Sample.SalesInvoices {
+    internal class PostInvoiceXml
+    {
+        internal static void Execute(Client client)
+        {
+            PostSalesInvoiceXml invoiceToPost = new PostSalesInvoiceXml();
+            invoiceToPost.Receiver.Name = "Company BV";
+            invoiceToPost.Receiver.EmailAddress.Name= "John Doe";
+            invoiceToPost.Receiver.EmailAddress.Address = "info@example.com";
+            invoiceToPost.Xml = "<YOUR_INVOICE_XML_GOES_HERE/>";
+            invoiceToPost.Image.FileName = "invoice.pdf";
+            invoiceToPost.Image.ContentType = "application/pdf";
+            invoiceToPost.Image.Base64Content = "<BASE64GOESHERE>";
+            Task<PostSalesInvoiceXmlResponse> task = Task.Run(
+                async () => await client.SalesInvoices.PostXmlAsync(invoiceToPost));
+            Console.WriteLine($"Invoice ID: ", task.Result.Id);
+        }
+    }
+}
