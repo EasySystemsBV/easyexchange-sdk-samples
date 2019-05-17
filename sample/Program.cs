82e5f80cb50079df673ce8269f5efc499e1a634c
amaters <amaters@icreative.nl>
2019-05-17 13:43:40 +0200

    SmpLookup toegevoegd

--- sample/Program.cs
+++ sample/Program.cs
@@ -5,14 +5,14 @@ namespace EasyExchange.Sdk.Core.Sample
     class Program
     {
         private const string ENDPOINT_URL = "https://easyexchange-partners.es-prd.net";
-        private const string CLIENT_ID = "IDYN";
-        private const string CLIENT_SECRET = "yjCJu2Xo8C3zjwynDpZfS8LT4lGVxMD1d2lu7G1c";
-        private const string USERNAME = "idyn";
-        private const string PASSWORD = "$2b$12$xbMrATn7CSxDVqlag9MEO.pdfEoH201OuX4gWnLrULvuysXtHrVaq";
+        private const string CLIENT_ID = "CLIENT";
+        private const string CLIENT_SECRET = "SECRET";
+        private const string USERNAME = "customer";
+        private const string PASSWORD = "password";
 
         static void Main(string[] args)
         {
-            Client client = new Client(ENDPOINT_URL, CLIENT_ID, CLIENT_SECRET, EasyExchangeScope.SalesInvoices);
+            Client client = new Client(ENDPOINT_URL, CLIENT_ID, CLIENT_SECRET);
             client.Authenticate( USERNAME, PASSWORD ).Wait();
 
             GetInvoices.Execute( client );
---
+++ sample/SmpLookup/Find.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyExchange.Sdk.Core.SalesInvoices.Models.GetImage;
+using EasyExchange.Sdk.Core.SmpLookup.Models.Find;
+
+namespace EasyExchange.Sdk.Core.Sample.SmpLookup
+{
+    internal class Find
+    {
+        internal static void Execute(Client client)
+        {
+            const string code = "0106";
+            const string value = "09160948";
+            Task<FindResponse> task = Task.Run(
+                async () => await client.SmpLookup.FindAsync(code, value));
+            Console.WriteLine($"Exists: ", task.Result.Exists);
+            Console.WriteLine($"#Supported Documents: ", task.Result.Participant.SupportedDocuments.Count);
+        }
+    }
+}
